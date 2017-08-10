97742c33602f9d81fc159bac27563c531f6dca4e
Kostiantyn4 <kostyantun4@hotmail.com>
2017-08-11 01:09:12 +0300

    Added recipe, food  and shopping lists to models and to UI.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.module';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Test Recipe',
+               'this is for test',
+               'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg')
+  ];
 
-  ngOnInit() {
+  constructor() {
+
+   }
+
+   ngOnInit(): void {
+
   }
 
 }
